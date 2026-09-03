8c6548c99e733814e0c9eb9129c8878297bf4fcc
JStaRFilms <johnoke2005@gmail.com>
2026-09-03 03:26:04 +0100

    fix(mobile): use Takomi branding in work log

--- apps/mobile/src/features/threads/thread-work-log.tsx
+++ apps/mobile/src/features/threads/thread-work-log.tsx
@@ -28,7 +28,7 @@ import {
 import Svg, { Defs, LinearGradient, Rect, Stop } from "react-native-svg";
 
 import { AppText as Text } from "../../components/AppText";
-import { T3Wordmark } from "../../components/T3Wordmark";
+import { TakomiWordmark } from "../../components/TakomiWordmark";
 import { cn } from "../../lib/cn";
 import { THREAD_WORK_ROW_MIN_HEIGHT, type deriveThreadWorkLogSizing } from "../../lib/layout";
 import type { ThreadFeedActivity } from "../../lib/threadActivity";
@@ -76,7 +76,7 @@ function WorkLogIcon(props: {
 }) {
   if (props.icon === "t3-code") {
     return (
-      <T3Wordmark
+      <TakomiWordmark
         height={10}
         {...(props.highlighted ? { colorClassName: "accent-foreground" } : { color: props.color })}
       />
